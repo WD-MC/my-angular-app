f351121c6ea285d82d3988af37727a12adeec4ea
WD-MC <wagsongmichele@gmail.com>
2025-02-05 12:19:34 +0100

    Update file

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,14 +1,26 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Lire le PID depuis .pidfile
-set /p pid=<.pidfile
+:: Vérifiez si le fichier .pidfile existe
+if exist .pidfile (
+    :: Lire le PID depuis .pidfile
+    set /p pid=<.pidfile
 
-:: Vérifier si le PID est valide
-powershell -Command "
-if (Get-Process -Id !pid! -ErrorAction SilentlyContinue) {
-    Stop-Process -Id !pid!
-    Write-Host 'Processus arrêté avec PID !pid!'
-} else {
-    Write-Host 'Aucun processus trouvé avec le PID !pid!'
-}"
+    :: Vérifiez si le PID est valide (existe et est un entier)
+    for /f "delims=" %%i in ('tasklist /FI "PID eq !pid!" 2^>nul') do (
+        set "line=%%i"
+    )
+
+    :: Si une ligne est trouvée (le processus existe), alors tuer le processus
+    if defined line (
+        echo 'Le processus avec PID !pid! est en cours d'exécution, tentative d'arrêt...'
+        taskkill /PID !pid! /F
+        echo 'Processus avec PID !pid! arrêté.'
+    ) else (
+        echo 'Aucune application trouvée avec le PID !pid!.'
+    )
+) else (
+    echo 'Le fichier .pidfile n'existe pas ou est vide.'
+)
+
+endlocal
