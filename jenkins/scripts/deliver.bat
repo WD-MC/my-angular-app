5a86faac32a8646a0aaef33db38cf10d7d417d7e
WD-MC <wagsongmichele@gmail.com>
2025-02-05 12:31:50 +0100

    Update file

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,7 +1,16 @@
 npm run ng build
-npm run ng serve &
-sleep 1
-echo %ERRORLEVEL% > .pidfile
+:: Lancer 'ng serve' en arrière-plan et récupérer son PID
+start /B npm run ng serve
 
+:: Attendre quelques secondes pour que le processus soit bien lancé
+timeout /t 1 > nul
+
+:: Récupérer le PID du processus 'ng serve'
+for /f "tokens=2" %%i in ('tasklist /fi "imagename eq node.exe" /fo csv ^| findstr /i "ng serve"') do set PID=%%i
+
+:: Écrire le PID dans le fichier .pidfile
+echo %PID% > .pidfile
+
+:: Afficher un message pour l'utilisateur
 echo 'Now...'
 echo 'Visit http://localhost:4200 to see your Node.js/Angular application in action.'
