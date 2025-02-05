1365fa9bae1950e29a5ca3f43f429f9c36e142af
WD-MC <wagsongmichele@gmail.com>
2025-02-05 12:24:20 +0100

    Update file

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,26 +1,5 @@
 @echo off
-setlocal enabledelayedexpansion
+set /p pid=<.pidfile
+echo PID récupéré: %pid%
 
-:: Vérifiez si le fichier .pidfile existe
-if exist .pidfile (
-    :: Lire le PID depuis .pidfile
-    set /p pid=<.pidfile
-
-    :: Vérifiez si le PID est valide (existe et est un entier)
-    for /f "delims=" %%i in ('tasklist /FI "PID eq !pid!" 2^>nul') do (
-        set "line=%%i"
-    )
-
-    :: Si une ligne est trouvée (le processus existe), alors tuer le processus
-    if defined line (
-        echo 'Le processus avec PID !pid! est en cours d'exécution, tentative d'arrêt...'
-        taskkill /PID !pid! /F
-        echo 'Processus avec PID !pid! arrêté.'
-    ) else (
-        echo 'Aucune application trouvée avec le PID !pid!.'
-    )
-) else (
-    echo 'Le fichier .pidfile n'existe pas ou est vide.'
-)
-
-endlocal
+taskkill /PID %pid% /F
