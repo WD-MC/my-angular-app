91d4bc0a4a4f94ec9d8b913f8867f6daaa433faf
WD-MC <wagsongmichele@gmail.com>
2025-02-05 12:42:07 +0100

    Update file

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,13 +1,1 @@
-@echo off
-set /p pid=<.pidfile
-echo PID récupéré: %pid%
-
-:: Vérification si le PID est valide
-tasklist /fi "PID eq %pid%" | findstr %pid% > nul
-if errorlevel 1 (
-    echo 'Le processus avec le PID %pid% n'existe pas ou a déjà été terminé.'
-    exit /b 1
-)
-
-taskkill /PID %pid% /F
-echo 'Processus avec le PID' %pid% 'arrêté.'
+taskkill /F /PID $(cat .pidfile)
