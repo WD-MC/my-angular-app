107a0e7b9f6ad5d71fb3b5362ed296d3b28f50ea
WD-MC <wagsongmichele@gmail.com>
2025-02-05 11:40:37 +0100

    Update file

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,11 +1,1 @@
-:: Lis le PID dans le fichier .pidfile
-for /f "tokens=*" %%i in (.pidfile) do set PID=%%i
-
-:: Vérifie si le PID est valide
-if not "%PID%"=="" (
-    :: Tuer le processus avec le PID
-    taskkill /PID %PID% /F
-    echo 'Process with PID %PID% has been killed.'
-) else (
-    echo 'No PID found. No process to kill.'
-)
+kill $(cat .pidfile)
